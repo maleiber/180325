8e20e3fb753a1e37306898df7af5f9bc459be9d1
maleiber <zhp266@sina.com>
2018-03-25 15:33:28 +0800

    create data builder which can make virtual data and wanna verify the digging algorithm thereafter

---
+++ data_builder.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 12:32:11 2018
+
+@author: 赵怀菩
+"""
+import random
+import math
+import numpy as np
+from draw_pic import *
+
+class data_builder(object):
+    def __init__(self,min_x_value,max_x_value,length,random_factor=0,commonfactor=1):
+        self.xlist=[]
+        self.randomlize_xlist=[]
+        self.min_x=min_x_value
+        self.max_x=max_x_value
+        self.length=length
+        self.random_factor=random_factor
+        self.common=commonfactor
+        self.build_array()
+        self._build_random_array()
+        #
+    def build_array(self):
+        if self.length<240:
+            print ('too small array: at least 240')
+            return
+        a=[x for x in range(self.length)]
+        #a is xaxis
+        b=self.max_x-self.min_x
+        k=b/2
+        
+        b=[(math.sin(i/12)+1)*k+self.min_x for i in a]
+        self.xlist=b
+        #b is value in yaxis
+        pass
+    def _build_random_array(self,array=False,start=0,end=False):
+        #start and end of this index
+        #random value is expand of the random series
+        if self.length<240:
+            print ('too small array: at least 240')
+            return
+        if array==False:
+            array=self.xlist
+        if end==False:
+            end=self.length
+            
+        temp_array=[]
+        if end-start<1:
+            return
+        rand=np.random.normal(size=end-start)
+        for i in range(end-start):
+            if abs(rand[i])>0.05:
+                #too common ,not changes
+                temp=((rand[i]*self.random_factor)+1)*array[start+i]
+                temp_array.append(temp)
+            else:
+                temp_array.append(array[start+i])
+        
+        #then do the total len change
+        randb=np.random.normal(size=end-start)
+        now=start
+        dx=self.max_x-self.min_x
+        for i in range(end-start):
+            if abs(randb[i])<1.5:
+                pass
+            elif i+start>=now:
+                #start line change
+                blocklength=int(rand[i]*0.6+100)
+                blocklength=min(blocklength,end-i)
+                blockshift=randb[i]*0.05*dx
+                for j in range(blocklength):
+                    temp_array[i+j]=temp_array[i+j]+blockshift
+                now=now+blocklength
+        
+        self.randomlize_xlist=temp_array
+        return temp_array
+        
+    
+if __name__=='__main__':
+    b=data_builder(20,30,600,0.025,1)
+    a=[x for x in range(600)]
+    draw_pic(b.randomlize_xlist,False,a)
+    pass
--- draw_pic.py
+++ draw_pic.py
@@ -10,41 +10,42 @@ import random
 import math
 
 class draw_pic(object):
-    def __init__(self,xarray,xcolor=False,yarray=False):
-        print (xarray,xcolor,yarray)
+    def __init__(self,yarray,xcolor=False,xarray=False,draw_line=True):
+        #value of yarray is necessary
+        print (yarray,xcolor,xarray)
         fig,ax = plt.subplots()
         #fig.set_size_inches(108.5, 10.5)
         ax.grid(True)
-        a=xarray
+        a=yarray
         cm = plt.cm.get_cmap('RdYlBu')
         if xcolor==False:
-            z=[250 for i in xarray]
+            z=[250 for i in yarray]
         else:
             z=xcolor
-        if yarray==False:
-            b=[i for i in range(len(xarray))]
+        if xarray==False:
+            b=[i for i in range(len(yarray))]
         else:
-            b=yarray
+            b=xarray
         sc = plt.scatter(b, a, c=z, vmin=0, vmax=256, s=35, cmap=cm)
-        ax.plot(b,a)
+        if draw_line==True:
+            ax.plot(b,a,alpha=0.2)
         plt.colorbar(sc)
         plt.show()
         pass
     
 if __name__ == '__main__':
-    a=[x for x in range(100)]
+    a=[x for x in range(500)]
     #a is xaxis
-    b=[math.sin(i/10) for i in a]
+    b=[math.sin(i/12) for i in a]
     #b is value in yaxis
     z=[random.randint(0,255) for i in a]
     #z is color of each point
-    cm = plt.cm.get_cmap('RdYlBu')
-    
-    
-    fig.set_size_inches(108.5, 10.5)
-    fig,ax = plt.subplots()
-    
-    sc = plt.scatter(a, b, c=z, vmin=0, vmax=256, s=35, cmap=cm)  
-    plt.colorbar(sc)  
-    plt.show()
+    sc = plt.scatter(a, b, c=z, vmin=0, vmax=256, s=35, cmap=cm)
+    x=[1,2,3,5]
+    y=[11,12,13,24]
+    xcolor=[20,20,232,223]
+    draw_pic(x)
+    draw_pic(x,xcolor)
+    draw_pic(x,xcolor,y)
+    draw_pic(x,xcolor,y,False)
         
